7ab2299a7e69d562fb5feb089af0a89f2070bf54
張天慈 <woodychang0611@gmail.com>
2021-06-13 23:12:44 +0800

    update

--- 04_post_process.py
+++ 04_post_process.py
@@ -8,28 +8,27 @@ from datetime import timedelta
 data = (  
     ('Single ETF', './data/etf_returns.csv'),    
     ('Monkey Portfolios', './data/random_portfolio_return.csv'),        
-    #('CRP', './data/constant_portfolio_return.csv'),
-    #('CRP with leverage', './data/l2_constant_portfolio_return.csv'),  
+    ('CRP', './data/constant_portfolio_return.csv'),
+    ('CRP with leverage', './data/l2_constant_portfolio_return.csv'),  
 )
 obs_period = 126 #6 months working days
 
 
 result_df = pd.DataFrame()
 for type,src in data:
-    df = pd.read_csv(src, parse_dates=True, index_col=0)
-    dates = list(d for d in df.index if (( d-df.index[0]).days >= obs_period))
+    ret_df = pd.read_csv(src, parse_dates=True, index_col=0)
+    dates = list(d for d in ret_df.index if (( d-ret_df.index[0]).days >= obs_period))
 
-    for date in dates[:2]:
+    for date in dates[:]:
         ed = date
         sd = date - timedelta(days=obs_period)
-        df = df[(df.index >=sd) & (df.index <=ed)]
+        df = ret_df[(ret_df.index >=sd) & (ret_df.index <=ed)]
         kpi_df = finance_utility.get_kpi(df,kpis=['cagr','std'])
         kpi_df['date'] = date
         kpi_df['type']=type
         kpi_df.set_index(['date','type','name'],inplace=True)  
         result_df =pd.concat([result_df, kpi_df])
-
-        print(kpi_df)
+        print(f'{type} {sd} {ed} {date}')
       
 print(result_df.index)
 result_df.to_csv('./data/kpis.csv')
